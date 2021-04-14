51046e3722d492b9e9a2f16f3e8336fb4b96128c
root <root@puppet.codespaces.io>
2021-04-14 15:12:30 +0000

    update manifests and hieradata

--- Puppetfile
+++ Puppetfile
@@ -2,16 +2,18 @@ forge "http://forge.puppetlabs.com"
 
 # Modules from the Puppet Forge
 # Versions should be updated to be the latest at the time you start
-#mod "puppetlabs/inifile",     '1.5.0'
-#mod "puppetlabs/stdlib",      '4.11.0'
+mod "puppetlabs/ntp",     '4.5.2'
+mod "puppetlabs/haproxy",      '1.5.0'
 #mod "puppetlabs/concat",      '2.1.0'
 
 # Modules from Git
 # Examples: https://github.com/puppetlabs/r10k/blob/master/doc/puppetfile.mkd#examples
-#mod 'apache',
-#  :git    => 'https://github.com/puppetlabs/puppetlabs-apache',
+mod 'tomcat',
+  :git    => 'https://github.com/johnsonkumara/puppet-module-tomcat.git',
 #  :commit => '83401079053dca11d61945bd9beef9ecf7576cbf'
 
+mod 'java',
+  :git    => 'https://github.com/johnsonkumara/puppet-module-java.git',
 #mod 'apache',
 #  :git    => 'https://github.com/puppetlabs/puppetlabs-apache',
 #  :branch => 'docs_experiment'
---
+++ manifests/app.pp
@@ -0,0 +1,40 @@
+node 'node1'{
+      include base
+      class {'::tomcat':
+         xms           => '60m',
+         xmx           => '120m',
+         user          => 'root',
+         group         => 'root',
+         service_state => 'running'
+      }
+      tomcat::deploy {"sysfoo":
+      deploy_url  => 'puppet:///modules/tomcat/sysfoo.war',
+
+      }
+     # include tomcat
+     # include prefs
+      notify {"***NODE1 Definition****":}
+}
+
+node 'node2'{
+     class { '::tomcat':
+         xms           => "70m",
+         xmx           => "130m",
+         user          => 'tomcat',
+         group         => 'tomcat',
+         service_state => 'running'
+      }
+     # include tomcat
+     # include prefs
+      notify {"***NODE2 Definition****":}
+}
+
+node default {
+ notify { 'checkpoint_1':
+  message => "
+
+  ****** Default node block*****
+  there is no definition decleared for this node
+  "
+ }
+}
